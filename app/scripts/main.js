8b6a1008717717fc85bbd9324ab3d99b183e42ef
iamike <dai.mike@gmail.com>
2015-12-03 12:21:29 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,20 +3,10 @@ $(function() {
   $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 0,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 5
-      }
-    }
+    items:1
   })
 
-  $('.count-board h5,').flowtype({
+  $('.count-board h5,.lastest-update p').flowtype({
     minimum: 320,
     maximum: 1200,
     minFont: 12,
