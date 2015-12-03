c13e35863e272653a2c77b59d122204c9417a0b0
iamike <dai.mike@gmail.com>
2015-12-03 12:14:44 +0800

    commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,28 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+$(function() {
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
     }
-})
+  })
+
+  $('.count-board h5,').flowtype({
+    minimum: 320,
+    maximum: 1200,
+    minFont: 12,
+    maxFont: 40,
+    fontRatio : 30
+
+  });
+
+});
