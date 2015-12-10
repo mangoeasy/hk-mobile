e19b7b64ae55537739ba36f5bc07e303026f86fd
Mike Dai <dai.mike@gmail.com>
2015-12-10 16:57:30 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,17 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 0,
+  items: 1,
+  autoplay: true,
+  autoplay: 2000
+})
+$('body').flowtype({
+  minimum: 320,
+  maximum: 640,
+  minFont: 12,
+  maxFont: 35,
+  fontRatio: 30
 
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 0,
-    items: 1,
-    autoplay:true,
-    autoplay : 2000
-  })
-  $('body').flowtype({
-    minimum: 320,
-    maximum: 640,
-    minFont: 12,
-    maxFont: 35,
-    fontRatio: 30
+});
 
-  });
+$('.count-board').scrollToFixed();
